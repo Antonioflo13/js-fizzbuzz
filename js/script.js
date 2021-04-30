fa2a343f6a5d69c6aecffb3ca6954a0815a460e5
Antonioflo13 <floreantonio@hotmail.it>
2021-04-30 09:34:46 +0200

    print to HTML

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
+var numeri = [];
 
 for (i = 1; i <= 100; i ++) {
     
     if (((i % 3) == 0 ) && ((i % 5) == 0)) {
+        numeri.push("FizzBuzz");
         console.log("FizzBuzz"); 
         
     } else if ((i % 3) == 0 ) {
+        numeri.push("Fizz");
         console.log("Fizz");
 
     } else if ((i % 5) == 0 ) {
+        numeri.push("Buzz");
         console.log("Buzz");
 
     } else {
+        numeri.push(i);
         console.log(i);
     } 
+}
+for (i = 0; i < 100; i ++) {
+
+    console.log(numeri[i]);
+    document.getElementById("result").innerHTML+= "<li>" + numeri[i] + "</li>";
 }
